8dc10351a6d62c3436fcb1fcc36ba3bce2286a10
Lucas Lisboa <lucaslisboalves@hotmail.com>
2025-03-23 10:06:36 -0500

    Initial Commit

---
+++ KeyLogger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KeyLogger
+//
+//  Created by Lucas Lisboa  on 3/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KeyLogger/KeyLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyLoggerApp.swift
+//  KeyLogger
+//
+//  Created by Lucas Lisboa  on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct KeyLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
